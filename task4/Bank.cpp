d53ccce2870cc22ddb8c92d63da3816779742eb0
coder-Retro <hasnainqadri9c@gmail.com>
2026-06-22 15:30:23 +0500

    Add: Initialized task 4

---
+++ task4/Bank.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+// Classes
+class Customer {
+private:
+
+public:
+
+};
+class Account {
+private:
+
+public:
+
+};
+class Transaction {
+private:
+
+public:
+
+};
+// Helper Function
+
+// Main Function
+int main() {
+
+    return 0;
+}
